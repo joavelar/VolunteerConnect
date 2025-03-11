1c49cf367f1788b6e7efcff3de3b7e3345819f8f
karllegson <klegson48@gmail.com>
2025-03-11 15:52:29 -0700

    feat(layout): added video background, filter, and center search bar

--- client/src/components/layout-component/layout-component.jsx
+++ client/src/components/layout-component/layout-component.jsx
@@ -12,7 +12,17 @@ const Layout = ({ children }) => {
       <video autoPlay muted loop className="video-background">
         <source src="/volunteerVideo.mp4" type="video/mp4" />
       </video>
-      <main>{children}</main>
+      <main>
+      <div className="content-center">
+        <h1 className="brand-title">VOLUNTEER CONNECT</h1>
+        <h2>Your Time. Their Future. Our Mission.</h2>
+          <div className="search-bar">
+            <input type="text" placeholder="Find City" />
+            <button type="button">Search</button>
+          </div>
+        </div>
+        {/* {children} */}
+      </main>
       <Footer />
     </div>
   );
