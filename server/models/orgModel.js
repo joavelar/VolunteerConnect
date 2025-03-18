cb034fe4e7a498da4875d86e018ea723362fea3e
Victor Lomeli <victorlomeli2023@gmail.com>
2025-03-17 19:51:03 -0700

    feat(postModel): created post endpoint

--- server/models/orgModel.js
+++ server/models/orgModel.js
@@ -1,6 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.sqlite');
 
+db.run("PRAGMA foreign_keys = ON;");
+
 // Create organizations table if not exists
 db.run(`
   CREATE TABLE IF NOT EXISTS organizations (
--- server/models/postModel.js
+++ server/models/postModel.js
@@ -22,3 +22,55 @@ db.run(`
       FOREIGN KEY (org_id) REFERENCES organizations(org_id) ON DELETE CASCADE
     )
   `);
+
+// Function to insert a new post
+const createPost = (org_id, post_title, post_location, post_date, post_time, post_image_1) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+      INSERT INTO posts (org_id, post_title, post_location, post_date, post_time, post_image_1)
+      VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    db.run(query, [org_id, post_title, post_location, post_date, post_time, post_image_1], function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this.lastID); // Return the ID of the newly created post
+      }
+    });
+  });
+};
+
+// Function to fetch all posts
+const getPosts = () => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM posts WHERE is_deleted = 0 ORDER BY created_at DESC';
+    
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
+
+const getOrganizationByUID = (firebase_uid) => {
+  return new Promise((resolve, reject) => {
+    db.get(
+      `SELECT * FROM organizations WHERE firebase_uid = ?`,
+      [firebase_uid],
+      (err, row) => {
+        if (err) {
+          reject(err); // Reject the promise if there's an error
+        } else {
+          resolve(row); // Resolve the promise with the row data
+        }
+      }
+    );
+  });
+};
+
+
+module.exports = { createPost, getPosts, getOrganizationByUID };
