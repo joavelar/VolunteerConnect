6fd708aef5771da20a68dec2be7f4fb35eea5a8a
Leo Masellis <leomasellis@gmail.com>
2025-03-27 12:46:23 -0700

    Feat(app.js) implemented React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
 import Layout from './components/layout-component/layout-component';
 import LandingPage from "./pages/landing-page/landing-page";
 import Login from "./pages/login-page/login-page";
@@ -8,10 +8,12 @@ export default function App() {
   return (
     <div>
       <Layout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
       </Layout>
     </div>
   );
