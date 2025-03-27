f2c01c723ecb02c8df5fbeeb1c624cd9ca0d183a
Leo Masellis <leomasellis@gmail.com>
2025-03-27 11:40:16 -0700

    WIP React Router Setup

--- client/src/components/layout-component/layout-component.jsx
+++ client/src/components/layout-component/layout-component.jsx
@@ -4,25 +4,11 @@ import Header from "../header-component/header-component";
 import Footer from "../footer-component/footer-component";
 import "./layout.css";
 
-const Layout = ({ children, }) => {
+const Layout = ({ children }) => {
   return (
     <div className="layout-container">
       <Header />
-      <div className="video-overlay"></div>
-      <video autoPlay muted loop className="video-background">
-        <source src="/volunteerVideo.mp4" type="video/mp4" />
-      </video>
-      <main>
-      <div className="content-center">
-        <h1 className="brand-title">VOLUNTEER CONNECT</h1>
-        <h2>Your Time. Their Future. Our Mission.</h2>
-          <div className="search-bar">
-            <input type="text" placeholder="Find City" />
-            <button type="button">Search</button>
-          </div>
-        </div>
-        {children}
-      </main>
+      {children}
       <Footer />
     </div>
   );
